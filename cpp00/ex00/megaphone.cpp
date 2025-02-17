fa6b783e5605ab1c1c7c36bc31909ba12c962ed4
punpumpensumpeeuuum <dinisconceicao2004@gmail.com>
2025-02-17 23:04:37 +0000

    by the eye of agamoto

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dinda-si <dinda-si@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 22:05:06 by dinda-si          #+#    #+#             */
+/*   Updated: 2025/02/10 22:05:06 by dinda-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j]; j++)
+		{
+			std::cout << static_cast<char>(std::toupper(argv[i][j]));
+		}
+	}
+	std::cout << "\n";
+	return 0;
+}
