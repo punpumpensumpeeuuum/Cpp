c75dfc4cab52fa159f1c0d91129e89d76978b08e
Dinis Da Silva E Ferreira Da Conceicao <dinda-si@c2r2s8.42lisboa.com>
2025-03-04 18:09:33 +0000

    bo2

---
+++ cpp03/ex01/ScravTrap.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScravTrap.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dinda-si <dinda-si@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 18:02:54 by dinda-si          #+#    #+#             */
+/*   Updated: 2025/03/04 18:03:50 by dinda-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCRAVTRAP_H
+# define SCRAVTRAP_H
+
+# include <iostream>
+# include <string>
+
+class ScavTrap
+{
+	private:
+		std::string			name;
+		int	unsigned		hp;
+		int	unsigned		energy;
+		int	unsigned		ad;
+	public:
+		
+};
+
+#endif
