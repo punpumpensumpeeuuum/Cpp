c10a4980b0332e5c5af25ba8a23f291514b0bbe8
punpumpensumpeeuuum <dinisconceicao2004@gmail.com>
2025-11-23 20:31:24 +0000

    7f eito

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -2,7 +2,74 @@
 # define ARRAY_H 
 
 # include <iostream>
+# include <exception>
+# include <cstdlib>
 
+template <typename T> class Array
+{
+	private:
+ 		T* arr;
+		size_t len;
+	public:
+		Array();
+		Array(unsigned int n);
+		Array(const Array& a);
+		Array& operator=(const Array& source);
+		T& operator[](unsigned int i);
+		~Array();
+		size_t	size() const
+		{
+			return (len);
+		}
+};
 
+template <typename T>
+Array<T>::Array()
+{
+	arr = NULL;
+	len = 0;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n)
+{
+	arr = new T[n];
+	len = n;
+}
+
+template <typename T>
+Array<T>::Array(const Array& a)
+{
+	arr = NULL;
+	*this = a;
+}
+
+template <typename T>
+Array<T> &Array<T>::operator=(const Array& a)
+{
+	if (this == &a)
+		return (*this);
+	if (this->arr)
+		delete[] arr;
+	len = a.size();
+	arr = new T[len];
+	for (size_t i = 0; i < len; i++)
+		arr[i] = a.arr[i];
+	return (*this);
+}
+
+template <typename T>
+T& Array<T>::operator[](unsigned int i) 
+{
+	if (i >= len)
+		throw std::exception();
+	return (arr[i]);
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+	delete[] arr;
+}
 
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -3,16 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dinda-si <dinda-si@student.42.fr>          +#+  +:+       +#+        */
+/*   By: buddy2 <buddy2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/22 16:16:23 by buddy2            #+#    #+#             */
-/*   Updated: 2025/11/21 15:56:06 by dinda-si         ###   ########.fr       */
+/*   Created: 2025/11/23 20:21:28 by buddy2            #+#    #+#             */
+/*   Updated: 2025/11/23 20:25:37 by buddy2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
-int main(void)
+int main()
 {
-
+	Array<int> numbers(20);
+	int* mirror = new int[20];
+	srand(time(NULL));
+	for (int i = 0; i < 20; i++)
+	{
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	for (int i = 0; i < 20; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "ERRO" << std::endl;
+			return (1);
+		}
+	}
+	try
+	{
+		std::cout << numbers[2] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		std::cout << numbers[-2] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		std::cout << numbers[20] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	delete [] mirror;
+	return 0;
 }
