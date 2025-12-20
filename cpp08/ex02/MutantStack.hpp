5e2380f80c7b3ff27eb035db579ff8dd5e64557d
punpumpensumpeeuuum <dinisconceicao2004@gmail.com>
2025-12-20 16:30:54 +0000

    corrigido

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -9,6 +9,16 @@ template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container>
 {
 	public:
+		MutantStack() : std::stack<T, Container>() {}
+		MutantStack(const MutantStack &other) : std::stack<T, Container>(other) {}
+		MutantStack &operator=(const MutantStack &other)
+		{
+			if (this != &other)
+				std::stack<T, Container>::operator=(other);
+			return *this;
+		}
+		~MutantStack() {}
+
 		typedef typename Container::iterator iterator;
 		iterator begin()
 		{
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: buddy2 <buddy2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 16:16:23 by buddy2            #+#    #+#             */
-/*   Updated: 2025/12/05 16:32:42 by buddy2           ###   ########.fr       */
+/*   Updated: 2025/12/20 16:30:35 by buddy2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ int main()
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
@@ -34,6 +33,15 @@ int main()
 		std::cout << *it << std::endl;
 		++it;
 	}
+	MutantStack<int> copy(mstack);
+	std::cout << "\nCopy top: " << copy.top() << std::endl;
+	MutantStack<int> assign;
+	assign = mstack;
+	std::cout << "Assign top: " << assign.top() << std::endl;
+	assign.pop();
+	std::cout << "After pop, new top: " << assign.top() << std::endl;
 	std::stack<int> s(mstack);
+	std::cout << "std::stack top: " << s.top() << std::endl;
+
 	return 0;
 }
