7c6d93ab1abae20df8e605643dd74477c6e6ff48
Dinis Da Silva E Ferreira Da Conceicao <dinda-si@c1r6s2.42lisboa.com>
2026-01-22 17:45:48 +0000

    tudo feitinho

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: dinda-si <dinda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 16:16:23 by buddy2            #+#    #+#             */
-/*   Updated: 2026/01/19 18:09:11 by dinda-si         ###   ########.fr       */
+/*   Updated: 2026/01/22 17:42:10 by dinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,22 +56,29 @@ void	BitcoinExchange::Input(std::string input)
 {
 	std::ifstream	file;
 	std::string		strfile;
+	std::string		mlt;
+	std::string		date;
 
 	file.open(input.c_str());
 	std::getline(file, strfile);
-	if (strfile == "date | value")
+	if (strfile == "date | value" )
 		std::getline(file, strfile);
 	while (!file.eof())
 	{
+		if (strfile.length() < 1)
+		{
+			std::cerr << "Error: empty line" << std::endl;
+			std::getline(file, strfile);
+			continue;
+		}
 		if (strfile.length() < 14)
-			std::cerr << "Wrong format" << std::endl;
-		std::string date = strfile.substr(0, 10);
-		std::string mlt = strfile.substr(13);
-		int key = 0;
+		{
+			std::cerr << "Error: bad input => " << strfile << std::endl;
+			return;
+		}
+		date = strfile.substr(0, 10);
+		mlt = strfile.substr(13);
 		float value = 0;
-		std::stringstream converter;
-		converter << date;
-		converter >> key;
 		std::stringstream cc;
 		cc << mlt;
 		cc >> value;
@@ -79,15 +86,41 @@ void	BitcoinExchange::Input(std::string input)
 			PrintOuput(date, value);
 		std::getline(file, strfile);
 	}
-	
+	if (strfile.length() < 1 && file.is_open())
+	{
+		std::cerr << "Error: empty line" << std::endl;
+		return;
+	}
+	if (strfile.length() < 14 && file.is_open())
+	{
+		std::cerr << "Error: bad input => " << strfile << std::endl;
+		return;
+	}
+	date = strfile.substr(0, 10);
+	mlt = strfile.substr(13);
+	float value = 0;
+	std::stringstream cc;
+	cc << mlt;
+	cc >> value;
+	if (Parsing(date, value, strfile) == 0)
+		PrintOuput(date, value);
+}
+
+bool isLeapYear(int year)
+{
+	if (year % 400 == 0)
+		return true;
+	if (year % 100 == 0)
+		return false;
+	return (year % 4 == 0);
 }
 
 int		BitcoinExchange::Parsing(std::string date, float value, std::string line)
 {
-    size_t idx = line.find("|");
+	size_t idx = line.find("|");
 	if ((line[idx + 1] != ' ' || line[idx - 1] != ' ') || (line.substr(4,1) != "-" && line.substr(7,1) != "-"))
 	{
-		std::cerr << "Invalid Format\n";
+		std::cerr << "Error: Invalid Format => " << line << std::endl;
 		return (-1);
 	}
 	int	year, month, day;
@@ -95,19 +128,21 @@ int		BitcoinExchange::Parsing(std::string date, float value, std::string line)
 	conv << date.substr(0,4) << " " << date.substr(5,2) << " " << date.substr(8,2);
 	conv >> year >> month >> day;
 	int month_limits[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	if (isLeapYear(year))
+		month_limits[1] = 29;
 	if (year < 2009 || month < 1 || month > 12)
 	{
-		std::cerr << "Invalid Date Format\n";
+		std::cerr << "Error: Invalid Date => " << line << std::endl;
 		return (-1);
 	}
 	if (day > month_limits[month - 1] || day < 1)
 	{
-		std::cerr << "Out of month range\n";
+		std::cerr << "Error: Out of month range => " << line << std::endl;
 		return (-1);
 	}
 	if (value < 0.00 || value > 1000.00 )
 	{
-		std::cerr << "Rate out of range\n";
+		std::cerr << "Error: Rate out of range => " << line << std::endl;
 		return (-1);
 	}
 	return (0);
@@ -129,12 +164,12 @@ void	BitcoinExchange::PrintOuput(std::string inputdate, float bitcoins)
 	}
 	if (flag == true)
 	{
-		std::cout << inputdate.insert(4,"-").insert(7,"-") << " => " << bitcoins << " = " <<  std::fixed << std::setprecision(2) << bitcoins * itb->second << "\n";
+		std::cout << inputdate << " => " << bitcoins << " = " <<  std::fixed << std::setprecision(2) << bitcoins * itb->second << "\n";
 		flag = false;
 	}
 	else
 	{
 		ite = this->database.lower_bound(inputdate);
-		std::cout << inputdate.insert(4,"-").insert(7,"-") << " => " << bitcoins << " = " << std::fixed << std::setprecision(2) << bitcoins * ite->second << "\n";
+		std::cout << inputdate << " => " << bitcoins << " = " << std::fixed << std::setprecision(2) << bitcoins * ite->second << "\n";
 	}
 }
